eb56cddf5dec48c5f36813db1f7ec7725bca4497
minhtblaser <trinhngocminh.9a1@gmail.com>
2020-12-21 19:11:58 +0700

    fraction

--- src/lessonClass/tempCodeRunnerFile.cpp
+++ src/lessonClass/tempCodeRunnerFile.cpp
@@ -1,1 +1,13 @@
-typedef
+int countStudentsHaveScholarship(Students source){
+//     int count = 0;
+//     for (int position = 0; position < source.amount; position++){
+//         if(source.students[position].gpa >= 7){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// void printAmountStudentsHaveScholarship(Studens source){
+//     int count = countStudentsHaveScholarship(source);
+//     printf("\n number of students have scholarship: %d", count);
+// }
